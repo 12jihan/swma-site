cdc728f8f7ac35863f5a0f3421b5cc7c484d2493
Jareem E. Hoff <jareem.e.hoff@gmail.com>
2018-06-06 16:57:45 -0400

    Just updated ~Hoek~ to -v 5.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainBannerComponent } from './components/landing-page-components/main-banner/main-banner.component';
+import { SectionsComponent } from './components/landing-page-components/sections/sections.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainBannerComponent,
+    SectionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
